e2043b3e8faac2304a71669c00bc9c0617ccc52a
makoto tsuyuki <mtsuyuki@gmail.com>
2019-02-14 23:03:05 +0900

    add test example

--- src/todo/tests.py
+++ src/todo/tests.py
@@ -1,3 +1,45 @@
-from django.test import TestCase
+from unittest.mock import patch
 
-# Create your tests here.
+from django.test import TestCase
+from django.test.utils import override_settings
+from factory.django import DjangoModelFactory
+from pytest import mark
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.test import APIClient
+from rest_framework.views import APIView
+
+from .models import Todo
+from .views import TodoViewSet
+
+
+class TodoFactory(DjangoModelFactory):
+    class Meta:
+        model = Todo
+
+
+@mark.django_db
+def test_list():
+    TodoFactory.create()
+    TodoFactory.create()
+
+    client = APIClient()
+    response = client.get('/todos', format='json')
+    assert 2 == len(response.data)
+
+
+# override_settings doesn't work
+# https://stackoverflow.com/questions/29367043/unit-testing-django-rest-framework-authentication-at-runtime
+# @override_settings(REST_FRAMEWORK={
+#     'DEFAULT_PERMISSION_CLASSES': (
+#         'rest_framework.permissions.IsAuthenticated',
+#     )
+# })
+@patch.object(TodoViewSet, 'permission_classes', new = [IsAuthenticated])
+@mark.django_db
+def test_list_permission():
+    TodoFactory.create()
+    TodoFactory.create()
+
+    client = APIClient()
+    response = client.get('/todos', format='json')
+    assert 403 == response.status_code
