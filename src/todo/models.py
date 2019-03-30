7d8647fde85860b5cf4c41376b603bd4240cd0e0
makoto tsuyuki <mtsuyuki@gmail.com>
2019-03-30 19:35:13 +0900

    add Comment model

--- src/todo/models.py
+++ src/todo/models.py
@@ -5,3 +5,12 @@ class Todo(models.Model):
     headline = models.CharField(max_length=200)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+class Comment(models.Model):
+    todo = models.ForeignKey(Todo, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = (('todo', 'title'),)
