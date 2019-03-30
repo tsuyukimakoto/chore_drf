0c99d6664937959776667a66fb2f404860bb6852
makoto tsuyuki <mtsuyuki@gmail.com>
2019-03-30 19:38:10 +0900

    restrict retrieve comments with captured todo id

--- src/todo/views.py
+++ src/todo/views.py
@@ -19,3 +19,6 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     queryset = Comment.objects.all().order_by('-created_at')
     serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        return Comment.objects.filter(todo_id=self.kwargs['todo_id'])
